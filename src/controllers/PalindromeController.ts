814e134ef525d616118881d03884f538860f6aea
Arthur <arthurarp@hotmail.com>
2022-06-11 14:24:12 -0300

    Change name of function
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- src/controllers/PalindromeController.ts
+++ src/controllers/PalindromeController.ts
@@ -2,8 +2,7 @@ import { Request, Response } from 'express';
 import { PalindromeService } from '../services/PalindromeService';
 
 export class PalindromeController {
-    async handle(request: Request, response: Response) {
-
+    async getAllPalindromes(request: Request, response: Response) {
         const palindromeService = new PalindromeService();
 
         const result = await palindromeService.getPalindromes(request.body);
